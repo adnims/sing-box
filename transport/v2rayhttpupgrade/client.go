9fa101f82c101c79b327cd6551b5ddf9ab8d8970
adnims <adnim.root@gmail.com>
2026-07-25 14:28:54 +0800

    Update client.go
    Signed-off-by: adnims <adnim.root@gmail.com>

--- transport/v2rayhttpupgrade/client.go
+++ transport/v2rayhttpupgrade/client.go
@@ -92,6 +92,9 @@ func (c *Client) DialContext(ctx context.Context) (net.Conn, error) {
 	}
 	request.Header.Set("Connection", "Upgrade")
 	request.Header.Set("Upgrade", "websocket")
+	if request.Header.Get("User-Agent") == "" {
+    	request.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36")
+	}
 	err = request.Write(conn)
 	if err != nil {
 		return nil, err
